88afa106242632a4ccb4bc502216860093a6200a
emmanuel-shlimon <shlimon.emmanuel@gmail.com>
2024-05-21 13:51:13 +0200

    Update index.php

--- jour1/job01/index.php
+++ jour1/job01/index.php
@@ -1,17 +1,20 @@
 <?php
-// Étape 1: Créer une variable nommée $str et lui affecter la valeur "LaPlateforme"
+
 $str = "LaPlateforme";
-
-// Étape 2: Afficher le contenu de la variable $str
-echo $str . "\n";
-
-// Étape 3: Créer une variable $str2 contenant "Vive"
 $str2 = "Vive";
-
-// Étape 4: Créer une variable $str3 contenant "!"
 $str3 = "!";
+echo $str2 . " " . $str . " " . $str3 . "\n";
 
-// Étape 5: Afficher le contenu des variables $str2, $str et $str3 ensemble
-echo $str2 . " " . $str . " " . $str3;
+$val = 6;
+echo $val . "\n";  
+$val += 4;
+echo $val . "\n";  
+
+$myBool = true;
+echo $myBool ? 'true' : 'false';  
+echo "\n";
+$myBool = false;
+echo $myBool ? 'true' : 'false'; 
+echo "\n";
+
 ?>
-
